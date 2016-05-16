24f7e31864b09b121537a37457ccfbd483f94962
Charles Howard <cdhoward@add123.com>
2016-05-16 07:21:48 -0400

    working on update endpoint

--- server/app.js
+++ server/app.js
@@ -1,6 +1,43 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/public'));
+var Techy = require('techy');
 
-console.log('started server on 8001');
+
+var webPath = __dirname + '/../web';
+var simpleGit = require('simple-git')(__dirname + '/../');
+
+
+
+function rebuildFiles(cb) {
+	Techy(webPath, cb);
+}
+
+
+function pull(cb) {
+	simpleGit.pull('origin', 'master', cb);
+}
+
+
+
+app.get('/update', function (req, res) {
+	res.send('updating...');
+
+
+	pull(function () {
+		console.log('done with pull');
+		rebuildFiles(function () {
+			console.log('done with rebuild');
+		});
+	});
+
+	
+
+});
+
+
+
+
+app.use(express.static(webPath + '/_dist'));
+
+console.log('started server on 8001   ', __dirname + '/../web/_dist');
 app.listen('8001');
