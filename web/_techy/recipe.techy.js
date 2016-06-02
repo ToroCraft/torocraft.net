6f3368773f973f60aa0cf4b2ffe32dbbcf0a1f60
Zane Riley <zeriley@gmail.com>
2016-06-01 22:51:01 -0400

    integrating sprite file

--- web/_techy/recipe.techy.js
+++ web/_techy/recipe.techy.js
@@ -4,19 +4,19 @@ function html(recipe) {
 <div class="recipe">
 	<div class="recipe-ingredients">
 		<div class="recipe-row">
-			<span class="recipe-slot ${recipe.slot1 || ''}"></span><span class="recipe-slot ${recipe.slot2 || ''}"></span><span class="recipe-slot ${recipe.slot3 || ''}"></span>
+			<span class="recipe-slot sprite item-sprite ${recipe.slot1 || ''}"></span><span class="recipe-slot sprite item-sprite ${recipe.slot2 || ''}"></span><span class="recipe-slot sprite item-sprite ${recipe.slot3 || ''}"></span>
 		</div>
 		<div class="recipe-row">
-			<span class="recipe-slot ${recipe.slot4 || ''}"></span><span class="recipe-slot ${recipe.slot5 || ''}"></span><span class="recipe-slot ${recipe.slot6 || ''}"></span>
+			<span class="recipe-slot sprite item-sprite ${recipe.slot4 || ''}"></span><span class="recipe-slot sprite item-sprite ${recipe.slot5 || ''}"></span><span class="recipe-slot sprite item-sprite ${recipe.slot6 || ''}"></span>
 		</div>
 		<div class="recipe-row">
-			<span class="recipe-slot ${recipe.slot7 || ''}"></span><span class="recipe-slot ${recipe.slot8 || ''}"></span><span class="recipe-slot ${recipe.slot9 || ''}"></span>
+			<span class="recipe-slot sprite item-sprite ${recipe.slot7 || ''}"></span><span class="recipe-slot sprite item-sprite ${recipe.slot8 || ''}"></span><span class="recipe-slot sprite item-sprite ${recipe.slot9 || ''}"></span>
 		</div>
 	</div>
 	<div class="recipe-arrow">
 	</div>
 	<div class="recipe-result">
-		<span class="recipe-slot slot-large ${recipe.result || ''}"></span>
+		<span class="recipe-slot slot-large sprite item-sprite ${recipe.result || ''}"></span>
 	</div>
 </div>
 `;
